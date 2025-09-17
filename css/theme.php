59b5cf26e1c499cdde834b4a0b7742403cca1c7f
2e0wat <155592050+2e0wat@users.noreply.github.com>
2025-09-17 11:45:58 +0100

    Create theme.php

---
+++ css/theme.php
@@ -0,0 +1,20 @@
+<?php
+
+    $bgclr  = '#D3D3D3'; // Main Background
+    $hdr	= '#FFFFFF'; // Header Background
+    $ftr	= '#151515'; // Footer Background
+    $pridk 	= '#0B3A78'; // Primary - Dark
+    $primid	= '#4075BC'; // Primary - Mid
+    $prilt	= '#CFDDEE'; // Primary - Light
+    $secdk	= '#76111E'; // Secondary - Dark
+    $secmid	= '#EB213B'; // Secondary - Mid
+    $seclt  = '#F7A6B1'; // Secondary - Light
+    $navtxt = '#FFFFFF'; // Navbar Text
+    $brdr   = '#838383'; // Table & Button Border
+    $tabhdr = '#FFFFFF'; // Table Header Text
+    $odrw   = '#FFFFFF'; // Table "Odd" Row
+    $buttxt	= '#000000'; // Filter Button Text
+    $errtxt = '#FF0000'; // Error Message Text
+    $errbg  = '#FFFFFF'; // Error Message Background
+        
+?>
